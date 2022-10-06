3ccd02c9aeafb792497a74f1d33ab3d5ecb3cb8b
Ceren Şahin <ceren.sahin04@gmail.com>
2022-10-06 11:05:47 +0300

    ...

--- app.js
+++ app.js
@@ -165,11 +165,6 @@ app.post("/handle", function(req, res) {
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started successfully.");
 })
